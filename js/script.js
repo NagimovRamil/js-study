8d2aa589ed8bc87041239150a97d763711b7f97d
Ramil Nagimov <ramil.nagimov@simbirsoft.com>
2022-07-25 14:19:34 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// 9 урок
+// ______________________________________
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2*4 == 8);
+console.log(2*4 === '8');
+
+// 8 урок
+// ______________________________________
+
+// const category = 'toys';
+//
+// console.log(`https://someurl.com/${category}`);
+
+// 2 урок
+// ______________________________________
+
+// // alert('Hello');
+//
+// // const result = confirm("Are you here?");
+// // console.log(result);
+//
+// // const answer = +prompt("Вам есть 18?", "18");
+// // console.log(typeof (answer));
+//
+// const answers = [];
+//
+// // answers[0] = prompt('Какое ваше имя?', '');
+// // answers[1] = prompt('Какая ваша фамилия?', '');
+// // answers[2] = prompt('Сколько вам лет?', '');
+//
+// // document.write(answers);
+// console.log(typeof (answers));
+
+// 1 урок
+// ______________________________________
+
+// let a = 15;
+//
+// let number = 5;
+// const leftBorderWidth = 1;
+//
+// number = 10;
+// console.log(number);
+//
+// var name = 'Ivan';
+//
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+//
+// console.log(obj.name);
+// console.log(obj["name"]);
+//
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
