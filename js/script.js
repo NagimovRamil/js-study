d1c78909dddc9177828a719245fd1ccbb57c2173
Ramil Nagimov <ramil.nagimov@simbirsoft.com>
2022-07-26 10:28:53 +0300

    018 task

--- js/script.js
+++ js/script.js
@@ -1,41 +1,73 @@
 "use strict";
 
+// 17 урок
+// ______________________________________
+
+// const str = "test";
+// const arr = [1, 2, 4];
+//
+// console.log(str.length);
+// console.log(arr.length);
+//
+// console.log(str[2]);
+//
+// console.log(str.toUpperCase());
+//
+// const fruit = "Some fruit";
+//
+// console.log(fruit.indexOf("fruit"));
+//
+// const logg = "Hello world";
+//
+// console.log(logg.slice(6, 11));
+// console.log(logg.slice(-5, -1));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6,5));
+//
+// const num = 12.2;
+// console.log(Math.round(num));
+//
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
 // 16 урок
 // ______________________________________
-let num = 20;
 
-function showFirstMessage(text) {
-    console.log(text);
-    // let num = 10;
-    console.log(num)
-}
-
-showFirstMessage("Hello world!");
-console.log(num);
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function () {
-  console.log("Hello")
-};
-
-logger();
-
-const calc = (a, b) => { return a + b };
+// let num = 20;
+//
+// function showFirstMessage(text) {
+//     console.log(text);
+//     // let num = 10;
+//     console.log(num)
+// }
+//
+// showFirstMessage("Hello world!");
+// console.log(num);
+//
+// function calc(a, b) {
+//     return (a + b);
+// }
+//
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+//
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+//
+// const anotherNum = ret();
+// console.log(anotherNum);
+//
+// const logger = function () {
+//   console.log("Hello")
+// };
+//
+// logger();
+//
+// const calc = (a, b) => { return a + b };
 
 // 14 урок
 // ______________________________________
