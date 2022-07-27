7a841f0262fda016aff1b34ab8f3ecfa50ca60eb
Ramil Nagimov <ramil.nagimov@simbirsoft.com>
2022-07-27 17:52:16 +0300

    024 task

--- js/script.js
+++ js/script.js
@@ -1,5 +1,245 @@
 "use strict";
 
+
+
+
+// 23 урок
+// ______________________________________
+
+// // let str = 'some';
+// // let strObj = new String(str);
+//
+// // console.log(typeof (str));
+// // console.log(typeof (strObj));
+// //
+// // console.dir([1,2,3]);
+//
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log("Hello");
+//     }
+// };
+//
+// // const john = {
+// //     health: 100
+// // };
+// //
+// // Object.setPrototypeOf(john, soldier);
+// // john.sayHello();
+//
+// const john = Object.create(soldier);
+//
+// john.sayHello();
+
+// 22 урок
+// ______________________________________
+
+// // let a = 5,
+// //     b = a;
+// //
+// // b = b + 5;
+// //
+// // console.log(b);
+// // console.log(a);
+// //
+// // const obj = {
+// //     a: 5,
+// //     b: 1
+// // };
+//
+// // const copy = obj;
+// //
+// // copy.a = 10;
+// //
+// // console.log(copy);
+// // console.log(obj);
+//
+// function copy(mainObj) {
+//     let objCopy = {};
+//
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//
+//     return objCopy;
+// }
+//
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+//
+// const newNumbers = copy(numbers);
+//
+// newNumbers.a = 10;
+//
+// // console.log(newNumbers);
+// // console.log(numbers);
+//
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+//
+// // console.log(Object.assign(numbers, add));
+//
+// const clone = Object.assign({}, add);
+//
+// clone.d = 20;
+//
+// // console.log(add);
+// // console.log(clone);
+//
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+//
+// newArray[1] = 'dfsfsfsfe';
+// // console.log(newArray);
+// // console.log(oldArray);
+//
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'livejournal', 'blogger'],
+//     internet = [...video, ...blogs, 'vk', 'facebook'];
+//
+// // console.log(internet);
+//
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+//
+// const num = [2, 5, 7];
+//
+// log(...num);
+//
+// const array = ['a', 'b'];
+//
+// const newAray = [...array];
+//
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+//
+// const newObj = {...q};
+
+// 21 урок
+// ______________________________________
+
+// const arr = [1, 2, 32, 6, 8, 10, 12];
+// arr.sort(compareNum);
+// console.log(arr);
+//
+//
+// function compareNum(a, b) {
+//     return a-b;
+// }
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function (value, index, array) {
+//     console.log(`${index}: ${value} внутри массива ${array}`);
+// });
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort(compareNum);
+// console.log(products.join('; '));
+
+
+
+// arr.pop();
+// arr.push(10);
+//
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+//
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// 20 урок
+// ______________________________________
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function () {
+//         console.log("Test");
+//     }
+// };
+//
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// options.makeTest();
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+//
+// delete options.name;
+//
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof (options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+// 19 урок
+// ______________________________________
+
+// function first() {
+//     setTimeout(function () {
+//        console.log(1);
+//     }, 500);
+// }
+//
+// function second() {
+//     console.log(2);
+// }
+//
+// first();
+// second();
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+//
+// function done() {
+//     console.log('Я прошел этот урок');
+// }
+//
+// learnJS('JavaScript', done);
+
 // 17 урок
 // ______________________________________
 
