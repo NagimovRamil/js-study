3601f0b1df1fd85a2345aad31d80bdd8b305058d
Ramil Nagimov <ramil.nagimov@simbirsoft.com>
2022-08-22 08:01:32 +0300

    039 task

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 "use strict";
 
-// 31 урок
+// 35 урок
 // ______________________________________
 
+const p = document.querySelectorAll('p');
+console.log(p);
 
-
+// 34 урок
+// ______________________________________
+//     // touchstart
+//     // touchmove
+//     // touchend
+//     // touchenter
+//     // touchleave
+//     // touchcancel
+//
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+//
+//     box.addEventListener('touchstart', (e) => {
+//         e.preventDefault();
+//
+//         console.log('Start');
+//     });
+//
+//     box.addEventListener('touchmove', (e) => {
+//         e.preventDefault();
+//
+//         console.log('Move');
+//     });
+// });
+//
+// // touches - список всех пальцев которые сейчас на экране
+// // targetTouches
+// // changedTouches
 
 // 29 урок
 // ______________________________________
