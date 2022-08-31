9271fe8ae8112b90efd8fc6331c8ce9402680768
Ramil Nagimov <ramil.nagimov@simbirsoft.com>
2022-08-31 13:56:40 +0300

    057 task

--- js/script.js
+++ js/script.js
@@ -1,5 +1,56 @@
 "use strict";
 
+// 57 урок
+// ______________________________________
+
+//filter
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+//
+// const shortNames = names.filter(function (name) {
+//    return name.length < 5;
+// });
+//
+// console.log(shortNames);
+
+//map
+
+// let answers = ['IvAn', 'AnnA', 'Hello'];
+//
+// answers = answers.map(item => item.toLowerCase());
+//
+// console.log(answers);
+
+//every/some
+
+// const some = [4, 'qwq', 'qdfsfdfsf'];
+//
+// // console.log(some.some(item => typeof(item) === 'number'));
+// console.log(some.every(item => typeof(item) === 'number'));
+
+//reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+// const res = arr.reduce((sum, current) => sum + current, 3);
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+// const obj = {
+//     ivan: 'person',
+//     ann: 'person',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
+//
+// const newArr = Object.entries(obj)
+//     .filter(item => item[1] === 'person')
+//     .map(item => item[0]);
+//
+// console.log(newArr);
+
 // 55 урок
 // ______________________________________
 
